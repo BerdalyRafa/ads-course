925cfa71ed15e72d6ca4bb98b056fb1e9743d1ec
админ <rafaberdali1@gmail.com>
2023-07-25 23:17:16 +0600

    4 task fsctorial number

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,30 @@ import java.util.Scanner;
 public class Main {
     // Function to check if a number is prime
 
-    public static boolean Prime(int n,int divisor) {
-        // Base case1 if the number is less than 2, it's not prime
-        if (n < 2) {
-            return false;
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
         }
-        // Base case2 if the divisor reaches the square root of n, n is prime
-        if (divisor> Math.sqrt(n)){
-            return true;
-        }
-        // If n is divisible by any number between 2 and sqrt(n), it's not prime
-        if (n%divisor == 0){
-            return false;
-        }
-        // check the next divisor, by the Recursively methd
-        return Prime(n,divisor+1);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("");
-        int numberToCheck = scanner.nextInt();
-        boolean PrimeNumber = Prime(numberToCheck,2);
-        //function to check if the input number is prime
-        if (PrimeNumber){
-            System.out.println( "prime");
-        }
-        else{
-            System.out.println( "composite");
+        System.out.print(" ");
+        int number = scanner.nextInt();
+
+        if (number < 0) {
+            System.out.println("not defined for negative numbers.");
+        } else {
+            int result = factorial(number);
+            System.out.println(number + "! = " + result);
         }
 
         scanner.close();
     }
 }
-
 
 
 
